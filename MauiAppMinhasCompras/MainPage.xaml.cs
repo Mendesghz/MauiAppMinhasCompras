ab41aaedb1b6a4f76fd5e8a58bd71a65817a2ed5
Mendesghz <Lucasbsantos2007@hotmail.com>
2025-09-07 22:55:51 -0300

    arrumando respositório

--- MauiAppMinhasCompras/MainPage.xaml.cs
+++ MauiAppMinhasCompras/MainPage.xaml.cs
@@ -1,56 +1,47 @@
-﻿using System.Collections.ObjectModel;
-
-namespace MauiAppMinhasCompras
+﻿namespace MauiAppMinhasCompras
 {
     public partial class MainPage : ContentPage
     {
-        // Lista original de produtos
-        private ObservableCollection<Produto> Produtos { get; set; }
+        int count = 0;
 
-        // Lista filtrada que será exibida na interface
-        public ObservableCollection<Produto> ProdutosFiltrados { get; set; }
+        // Agora é Button, não mais object
+        private Button CounterBtn;
 
         public MainPage()
         {
+            // Inicializa os componentes do XAML
             InitializeComponent();
 
-            // Inicializa lista com alguns produtos
-            Produtos = new ObservableCollection<Produto>
+            // Caso não esteja usando XAML, crie o botão aqui:
+            CounterBtn = new Button
             {
-                new Produto { Nome = "Arroz 5kg" },
-                new Produto { Nome = "Feijão Carioca" },
-                new Produto { Nome = "Macarrão Espaguete" },
-                new Produto { Nome = "Azeite de Oliva" },
-                new Produto { Nome = "Leite Integral" },
-                new Produto { Nome = "Café Torrado" },
-                new Produto { Nome = "Detergente Líquido" },
-                new Produto { Nome = "Sabão em Pó" }
+                Text = "Click me"
             };
 
-            // Inicialmente mostra todos
-            ProdutosFiltrados = new ObservableCollection<Produto>(Produtos);
+            CounterBtn.Clicked += OnCounterClicked;
 
-            BindingContext = this;
-        }
-
-        // Evento disparado quando o texto do SearchBar muda
-        private void OnSearchBarTextChanged(object sender, TextChangedEventArgs e)
-        {
-            var textoBusca = e.NewTextValue?.ToLower() ?? string.Empty;
-
-            ProdutosFiltrados.Clear();
-
-            foreach (var produto in Produtos)
+            // Adiciona o botão na tela
+            Content = new StackLayout
             {
-                if (produto.Nome.ToLower().Contains(textoBusca))
-                    ProdutosFiltrados.Add(produto);
-            }
+                Children = { CounterBtn }
+            };
         }
-    }
 
-    // Classe simples para representar produto
-    public class Produto
-    {
-        public string Nome { get; set; }
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
     }
 }
